dce95982a1cc815a45b162a1e0c7492051174ec2
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-10-03 01:04:32 -0700

    Building filter Products page and adding logic using Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import Main from "./Components/Main/Main"
+import Main from "./Components/Main/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FilteredProducts from './Components/FilteredProducts/FilteredProducts';
 
 function App() {
   return (
     <div className="App">
-       <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Main />}></Route>
+          <Route path="/filteredProducts/:type" element={<FilteredProducts />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import sliderReducer from "../features/slices/sliderSlice";
+import productReducer from '../features/slices/productsSlice';
+import thunk  from "redux-thunk";;
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
+    products: productReducer,
   },
-});
+},  applyMiddleware(thunk));
+
+
